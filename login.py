f59626248256302e33d52cf7fcbe7824430f00fd
wielder994 <ranarajat994@gmail.com>
2026-09-12 09:56:30 +0530

    Initial commit

--- login.py
+++ login.py
@@ -1,2 +1,11 @@
 print("Log in Rajat")
 print("please enter the credentials")
+
+# cricket game 
+
+# Player 1 details:
+print("Enter the details of player 1")
+p1_name = input("Player 1 name:")
+
+print("Enter the details of player 2")
+
